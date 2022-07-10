9d167d5ff4779e188e69c5ef14699a477e24e98d
Tomas Pilar <thomas7pilar@gmail.com>
2022-07-10 16:25:20 +0200

    v1.1.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="trezor-pass",
-    version="1.1.0",
+    version="1.1.1",
     description="Trezor Password Manager Command-Line interface",
     long_description=long_description,
     long_description_content_type="text/markdown",
